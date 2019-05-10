f9518ed299305f572ab0264fdaf8174201066ba7
MicDMar <martmic2@bvu.edu>
2019-05-09 22:55:45 -0500

    Added finger table funtionality and keep it healthy.

--- peers.py
+++ peers.py
@@ -1,6 +1,45 @@
 from hash_functions import get_hash, hash_size
 
+from client import owns, pulse, open_connection
+from logging import debug
 from net_functions import *
+from threading import Thread
+import random
+from hash_functions import hash_size
+from time import sleep
+
+MAX_TABLE_SIZE = 5
+TABLE_OFFSET_PERCENT = 0.2
+
+def handle_table(table):
+    #Have this function always running to maintian a healty finger table.
+    while True:
+        #Check to see if each peer in the table is alive or not.
+        for peer in table:
+            conn = open_connection(peer)
+            if pulse(peer.address) == False:
+                table.remove(peer.addresss)
+        
+        #Find a random offset and add the peer to our finger table if they aren't already in there
+        for i in range(0, table.size-len(table.table))
+            rand = random.randint(0, hash_size())
+            table_offset = hash_size()*TABLE_OFFSET_PERCENT
+            
+            bottom = table.us.hash-table_offset
+            top = table.us.hash+table_offset
+            
+            while bottom < rand < top:
+                rand = random.randint(0, hash_size())
+           
+            peer_to_add = owns(rand)
+
+            #This check makes sure peers aren't added twice in our finger table.
+            if table.contains(peer_to_add):
+                continue
+            else:
+                table.add_address(peer_to_add)
+
+        sleep(10)
 
 class Peer:
     def __init__(self, address, port):
@@ -9,22 +48,28 @@ class Peer:
 
     def __repr__(self):
         return "<Peer: {} : {}>".format(self.address, self.hash)
-
+    
 class FingerTable:
-    def __init__(self, our_address, port, size=5):
+    def __init__(self, our_address, port, size=MAX_TABLE_SIZE):
         self.us = Peer(our_address, port)
         self.size = size
         self.table = [self.us]
+        Thread(target=handle_table, args=(self,))
         self.successors = []
         self.predecessor = None
 
     def get(self, hsh):
-        options = [x for x in self.table if x.hash < hsh]
+        options = [x for x in self.table if x.hash <= hsh]
         if len(options) == 0:
-            return self.us
+            return max(self.table, key=lambda p: p.hash)
         else:
             # Find the 'closest' owner of this hash
             return max(options, key=lambda p: p.hash)
+    
+    def contains(addr):
+        return len([x for x in self.table if x.address == addr]) >= 1
+
+        
 
     def add(self, peer):
         # FIXME: Enforce size limit, remove items if needed
